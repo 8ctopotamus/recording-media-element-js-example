319280c1508e404c0e17e9699a123927e93720c8
Josh <zylo.codes@gmail.com>
2024-09-15 09:10:39 -0500

    capture user media event listeners, css

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const preview = document.getElementById("preview");
-const recording = document.getElementById("recording");
-const startButton = document.getElementById("startButton");
-const stopButton = document.getElementById("stopButton");
-const downloadButton = document.getElementById("downloadButton");
-const logElement = document.getElementById("log");
+const preview = document.getElementById('preview')
+const recording = document.getElementById('recording')
+const startButton = document.getElementById('startButton')
+const stopButton = document.getElementById('stopButton')
+const downloadButton = document.getElementById('downloadButton')
+const logElement = document.getElementById('log')
 
-const lengthTimeMS = 5000;
-const recordingTimeMS = 5000;
+const lengthTimeMS = 5000
+const recordingTimeMS = 5000
 
 function log(msg) {
   logElement.innerText += `${msg}\n`
@@ -39,5 +39,35 @@ function startRecording(stream, lengthTimeMS) {
 }
 
 function stop(stream) {
-  stream.getTracks().forEach(track => track.stop())
+  stream?.getTracks()?.forEach(track => track.stop())
 }
+
+const handleStartButtonClick = () => 
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then(stream => {
+      preview.srcObject = stream
+      downloadButton.href = stream
+      preview.captureStream = preview.captureStream || preview.mozCaptureStream
+      return new Promise(resolve => preview.onplaying = resolve)
+    })
+    .then(() => startRecording(preview.captureStream()))
+    .then(recordedChunks => {
+      const recordedBlob = new Blob(recordedChunks, { type: 'video/webm' })
+      recording.src = URL.createObjectURL(recordedBlob)
+      downloadButton.href = recording.src
+      downloadButton.download = 'recorded-video.webm'
+      log(`Successfully recorded ${recordedBlob.size} bytes ${redcordedBlog.type} media.`)
+    })
+    .catch(error => {
+      if (error.name === 'NotFoundError') {
+        log('Camera or microphone not found. Can\'t record.')
+      } else {
+        log(error)
+      }
+    })
+
+const handleStopButtonClick = () => stop(preview.srcObject)
+
+startButton.addEventListener('click', handleStartButtonClick, false)
+stopButton.addEventListener('click', handleStopButtonClick, false)
